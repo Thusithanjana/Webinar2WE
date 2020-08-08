e019b820721d480912876cec6e3d1f48a347305e
Thusithanjana <thusithanjana@gmail.com>
2020-08-08 11:03:58 +0530

    Changed by Thusithanjana

--- app/src/main/java/com/example/usingstrings/MainActivity.java
+++ app/src/main/java/com/example/usingstrings/MainActivity.java
@@ -3,17 +3,52 @@ package com.example.usingstrings;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause: ");
         TextView tv1 = findViewById(R.id.TvMsg2);
         tv1.setText(R.string.Msg2);
+        Log.i(TAG, "onCreate: invoked");
 
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop: ");
     }
 }
